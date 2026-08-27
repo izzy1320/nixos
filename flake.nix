d9da4e0a6fff17bb8ba4e66c2d56d2b790c58793
izzy1320 <izineth@proton.me>
2026-08-27 17:23:09 -0400

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+   description = "A very basic flake";
+   inputs = {
+     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+   };
+   
+   outputs = { self, nixpkgs, home-manager, ... }: {
+       nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+         modules = [
+           ./configuration.nix
+           home-manager.nixosModules.home-manager
+           {
+             home-manager = {
+               useGlobalPkgs = true;
+               useUserPackages = true;
+               users.izzy = import ./home.nix;
+               backupFileExtension = "backup";
+            };
+         ];
+       };
+     };
+  };
+}
