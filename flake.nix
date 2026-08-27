f74bd78da89e97d9dab229690437fd119f5bbc7d
izzy1320 <izineth@proton.me>
2026-08-27 17:33:54 -0400

    Update flake.nix
    Cleaned up code and fixed the braces

--- flake.nix
+++ flake.nix
@@ -1,23 +1,25 @@
 {
-   description = "A very basic flake";
-   inputs = {
-     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-   };
-   
-   outputs = { self, nixpkgs, home-manager, ... }: {
-       nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
-         modules = [
-           ./configuration.nix
-           home-manager.nixosModules.home-manager
-           {
-             home-manager = {
-               useGlobalPkgs = true;
-               useUserPackages = true;
-               users.izzy = import ./home.nix;
-               backupFileExtension = "backup";
-            };
-         ];
-       };
-     };
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.izzy = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
   };
 }
