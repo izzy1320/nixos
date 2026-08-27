02d4c04a3618d0a8aa46c7645198dcfe80690e10
izzy1320 <izineth@proton.me>
2026-08-27 19:36:19 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,28 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.izzy = import ./home.nix;
-            backupFileExtension = "backup";
-          };
-        }
-      ];
+   outputs = { self, nixpkgs, ...}: {
+     nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+           ./configuration.nix
+        ];
+      };
     };
-  };
 }
